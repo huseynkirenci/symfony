d8f0c89bc7f4e782a91b897a836149df789ae75a
Hüseyin Kirenci <huseyinkrnc@gmail.com>
2024-09-17 09:27:41 +0300

    ders video

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -14,33 +14,14 @@ class MicroPostController extends AbstractController
     #[Route('/micro/post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-        // Veri Silme
-        $microPost = $posts->find(8);
-        $posts->remove($microPost, true);
-        
-        // Güncelleme
-        #$microPost = $posts->find(7);  // ID'si 7 olan MicroPost kaydını bul
-        #if ($microPost) {
-        #    $microPost->setTitle('Welcome in General!');  // Başlığı güncelle
-         #   $posts->save($microPost, true);  // Değişiklikleri kaydet
-        #}
-
-        // Bu kısımda veri tabanına yeni eklemeler yaptık
-        #$microPost = new MicroPost();
-        #$microPost->setTitle('It comes from controller');  ## MicroPost nesnesine başlık atanıyor
-        #$microPost->setCreated(new DateTime());  ## MicroPost nesnesine oluşturulma tarihi atanıyor
-
-        #$posts -> save($microPost, true);
-
-
-        // Bu Kısımda Sorgulama İşlemlerinin Fonksiyonlarını İnceledik.
-        # dd($posts->findAll()); # Tüm MicroPost kayıtlarını döndürür.
-        # dd(vars: $posts->find(1)); # ID'si 1 olan MicroPost kaydını döndürür.
-        # dd(vars: $posts->findOneBy(['title' => 'Welcome to Çorum!'])); # 'Welcome to Çorum!' başlıklı ilk MicroPost kaydını döndürür.
-        ## dd(vars: $posts->findBy(['title' => 'Welcome to Çorum!'])); # 'Welcome to Çorum!' başlığına sahip tüm MicroPost kayıtlarını döndürür.
+        dd($posts->findAll());
         
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
+    }
+    #[Route('/micro/post/{id}', name: 'app_micro_post_show')]
+    public function showOne($id, MicroPostRepository $posts): Response{
+        dd($posts->find($id));
     }
 }
