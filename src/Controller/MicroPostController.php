14dca11153c6afa38ce41e50372ee6f80079f947
Hüseyin Kirenci <huseyinkrnc@gmail.com>
2024-09-23 15:24:03 +0300

    Bu kısımda yeni özelleştirebilir formlar oluşturdum ve doctrine de 1'e bir ilişkiye yeni geçiş yaptım.

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -77,7 +77,7 @@ class MicroPostController extends AbstractController
         }
 
         return $this->render(
-            'micro_post/add.html.twig',
+            'micro_post/edit.html.twig',
             [
                 'form' => $form
             ]
