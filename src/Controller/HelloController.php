8690f9060ff7665dbe43dc97c07642a2da8d0da6
Hüseyin Kirenci <huseyinkrnc@gmail.com>
2024-09-25 12:10:37 +0300

    65-68 ders araları bu derslerde oneToone ile oneTomany yaptık çoğunu yorum satırı olarak geri çevirdim. ama genel mantığı aynı

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Entity\User;
 use App\Entity\Deneme;
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Entity\UserProfile;
+use App\Repository\MicroPostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserProfileRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,16 +22,71 @@ class HelloController extends AbstractController
         ['message' => 'Hi', 'created' => '2024/07/10'],
         ['message' => 'Bye!', 'created' => '2023/08/10']
     ];
-    #[Route('/{limit<\d+>?3}', name: 'app_index')]
-    public function index(int $limit,EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'app_index')]
+    public function index(MicroPostRepository $posts, UserProfileRepository $profiles,  EntityManagerInterface $entityManager): Response
     {
-        //$a = $entityManager->getRepository(Deneme::class)->findAll();
-        
+        /*  Bu kısımda onetomany ile ilgili veri ekleme işlemi yaptık
+            // Yeni bir post oluşturuyoruz
+            $post = new MicroPost();
+            $post->setTitle('Hello World!');
+            $post->setText('This is the content of the post.');
+            $post->setCreated(new DateTime());
+
+            // Yeni bir yorum oluşturuyoruz
+            $comment = new Comment();
+            $comment->setText('This is a comment for the post.');
+
+            // Posta yorumu ekliyoruz
+            $post->addComment($comment);
+
+            // Post'u ve ilişkilendirilen yorumları veritabanına kaydediyoruz
+            $entityManager->persist($post);  // Bu işlem comment'i de otomatik olarak kaydeder
+            $entityManager->flush();  // Değişiklikler veritabanına yazılır
+        */
+        /* İd numarası belli olan verileri silme işlemi
+        $post = $posts -> find(42); # id numarasına dikkat et
+        $comment = $post -> getComments()[0];
+        $comment -> setPost(null)
+        $post -> removeComment($comment);
+        // Post'u ve ilişkilendirilen yorumları veritabanına kaydediyoruz
+        $entityManager->persist($post);  // Bu işlem comment'i de otomatik olarak kaydeder
+        $entityManager->flush();  // Değişiklikler veritabanına yazılır
+        */
+        /*
+            $user = new User();
+            $user -> setEmail('email@email.com');
+            $user -> setPassword('12345678');
+            $profile = new UserProfile();  // Yeni UserProfile nesnesi oluşturuluyor
+            $profile ->setUser($user);
+            $entityManager->persist($profile);  // EntityManager bu nesneyi yönetir ve kaydetmeye hazırlar
+            $entityManager->flush();  // Değişiklikler veritabanına yazılır
+        */
+        // Önce user_id'ye göre user_profile tablosundan ilgili kaydı buluyoruz
+
+        /*
+        $profile = $profiles->find(['user' => 11]);  // $id, user tablosundaki kullanıcı id'si
+
+        if ($profile) {
+            // Eğer bu user_id'ye sahip bir profil varsa, EntityManager bu nesneyi silmeye hazırlar
+            $entityManager->remove($profile);
+
+            // Değişiklikler veritabanına yazılır ve $profile kaydı silinir
+            $entityManager->flush();
+
+            // Silme işlemi başarılı mesajı
+            $this->addFlash('success', 'Profile has been successfully deleted.');
+        } else {
+            // Eğer user_id'ye sahip bir profil bulunamazsa hata mesajı döner
+            $this->addFlash('error', 'Profile not found.');
+        }
+        */
+
+
         return $this->render(
-            'hello/index.html.twig', 
+            'hello/index.html.twig',
             [
-                'messages' => $this -> messages,
-                'limit' => $limit
+                'messages' => $this->messages,
+                'limit' => 3
             ]
         );
     }
@@ -32,7 +94,9 @@ class HelloController extends AbstractController
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
     public function showOne(int $id): Response
     {
-        return $this->render("hello/show_one.html.twig",['message' => $this->messages[$id]]
+        return $this->render(
+            "hello/show_one.html.twig",
+            ['message' => $this->messages[$id]]
         );
     }
 }
---
+++ src/Repository/CommentRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Comment;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Comment>
+ */
+class CommentRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Comment::class);
+    }
+    public function save(Comment $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    // Mikro post için ayrı bir save metodu da ekleyebilirsiniz.
+    public function saveMicroPost(MicroPost $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    //    /**
+    //     * @return Comment[] Returns an array of Comment objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Comment
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
