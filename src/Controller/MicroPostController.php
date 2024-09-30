c5440d119ebd38b81bdc9ed2a87ed2fb4593e5c6
Hüseyin Kirenci <huseyinkrnc@gmail.com>
2024-09-30 21:13:46 +0300

    Ders83: Bu dersin çıktısıi ile benim çıktım hatalı ama dursun bir sıkıntı var çözülemeyen yoksa eğer kullanıcılar arasında yetkiler bazı durumlarda yetersizdi bu yüzden ek yetkiler verilip alınma işlemiyapıldı

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -29,6 +29,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[IsGranted(MicroPost::VIEW, 'post')]
     public function showOne(MicroPost $post): Response
     {
         return $this->render('micro_post/show.html.twig', [
@@ -63,7 +64,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
-    #[IsGranted('ROLE_EDITOR')]
+    #[IsGranted(MicroPost::EDIT, 'post')]
     public function edit(MicroPost $post, Request $request, MicroPostRepository $posts, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createFormBuilder($post)
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -2,9 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\CommentRepository;
+use DateTime;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CommentRepository;
 use Symfony\Component\Validator\Constraints as Assert;
 
 
