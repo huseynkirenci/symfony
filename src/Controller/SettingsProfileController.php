4d49ab76de17d64289a6ed2135343257995c51eb
Hüseyin Kirenci <huseyinkrnc@gmail.com>
2024-10-02 13:15:57 +0300

    Ders90: Bu derste profile settingsi ana ekrana getirdik.

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use Doctrine\ORM\EntityManager;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +17,15 @@ class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
     public function profile(
-        Request $request
+        Request $request,
+        UserRepository $users,
+        EntityManagerInterface $entityManager
     ): Response {
         /** @var User $user */
         $user = $this->getUser();
         $userProfile = $user->getUserProfile() ?? new UserProfile();
+        $entityManager->persist($user);
+        $entityManager->flush();
 
         $form = $this->createForm(
             UserProfileType::class,
@@ -28,6 +35,16 @@ class SettingsProfileController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userProfile = $form->getData();
+            $user->setUserProfile($userProfile);
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Your user profile settings were saved'
+            );
+
+            return $this->redirectToRoute(
+                'app_settings_profile'
+            );
             // Save this somehow
             // Add the flash message
             // Redirect
