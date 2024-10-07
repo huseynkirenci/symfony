cd37ed8a74bc99054955cf0471eee3e9992c1d25
Hüseyin Kirenci <huseyinkrnc@gmail.com>
2024-10-07 16:08:50 +0300

    DERS104-DERS105: Bu derslerde resim ekleme yeri oluşturduk.

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use App\Form\ProfileImageType;
 use Doctrine\ORM\EntityManager;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SettingsProfileController extends AbstractController
@@ -54,6 +56,21 @@ class SettingsProfileController extends AbstractController
             'settings_profile/profile.html.twig',
             [
                 'form' => $form->createView(),
+            ]
+        );
+    }
+
+    #[Route('/settings/profile-image', name: 'app_settings_profile_image')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profileImage(): Response
+    {
+        $form = $this->createForm(ProfileImageType::class);
+
+        return $this->render(
+            'settings_profile/profile_image.html.twig',
+            [
+                'form' => $form->createView(),
+
             ]
         );
     }
